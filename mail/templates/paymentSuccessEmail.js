abf5fbea4f783a7ea879072361f356b5244c4145
Mohammad <mohammadabdullateef@eng.rizvi.edu.in>
2023-08-14 01:27:51 +0530

    email changed in template

--- mail/templates/paymentSuccessEmail.js
+++ mail/templates/paymentSuccessEmail.js
@@ -77,7 +77,7 @@ exports.paymentSuccessEmail = (name, amount, orderId, paymentId) => {
                   <p>Your Order ID is <b>${orderId}</b></p>
               </div>
               <div class="support">If you have any questions or need assistance, please feel free to reach out to us at <a
-                      href="mailto:info@studynotion.com">info@studynotion.com</a>. We are here to help!</div>
+                      href="mailto:onfirefreelancer@gmail.com">onfirefreelancer@gmail.com</a>. We are here to help!</div>
           </div>
       </body>
       
